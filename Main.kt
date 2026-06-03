f1ca9133097a4d31f3206c5941ae81af0b85d635
Aditya Gupta <1387guptaditya@gmail.com>
2026-06-03 14:03:04 +0530

    Rename main function to ararylearning and update logic

--- Main.kt
+++ Main.kt
@@ -1,11 +1,12 @@
-fun main() {
+fun ararylearning() {
     val names = arrayOf("adi", "ram", "avi", "hey")
     val num = arrayOf(1, 2, 3, "name", 'a')
+  
 
     for (name in names)
         println("name stores in names array ${name}")
 
     for (n in num)
 //        println("differet type of elements in array ${n}")
-        println(n)
+         println(n)//
 }
